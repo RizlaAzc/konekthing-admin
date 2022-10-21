55b463602954bd7adf3fe4a0c75ffa11b358e9ed
siska <siskaramadhani1710@gmail.com>
2022-10-21 10:44:51 +0700

    r

--- konekthing (4).sql
+++ konekthing (4).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 21 Okt 2022 pada 05.34
+-- Waktu pembuatan: 21 Okt 2022 pada 05.43
 -- Versi server: 10.4.21-MariaDB
 -- Versi PHP: 7.4.24
 
